9bb388942c3715ae3538496ebdf3ae74a8e20bd2
tristelme <francojulieta78@gmail.com>
2024-08-06 11:07:47 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { MatCardModule } from '@angular/material/card';  
+// import { AppComponent } from './app.component';
+// import { CardComponent } from './modules/inicio/component/card/card.component';
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     CardComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule,
+//     MatCardModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
---
+++ src/app/modules/inicio/component/card/card.component.ts
@@ -0,0 +1,37 @@
+// import { Component } from '@angular/core';
+// //IMPORTAMOS LA INTERFAZ PARA CIUDAD
+// @Component({
+//   selector: 'app-card',
+//   templateUrl: './card.component.html',
+//   styleUrls: ['./card.component.css']
+// })
+// export class CardComponent {
+//   //PROPIEDAD PUBLICA --> TIPO ARRAY
+//   public info:[];
+
+//   //INICIALIZAR LA PROPIEDAD INFO
+//   constructor() {
+//     this.info = [
+//       {
+//         id: "",
+//         nombre: "",
+//         imagen: "",
+//         alt: ""
+//       },
+
+//       {
+//         id: "",
+//         nombre: "",
+//         imagen: "",
+//         alt: ""
+//       },
+
+//       {
+//         id: "",
+//         nombre: "",
+//         imagen: "",
+//         alt: ""
+//       },
+//     ]
+//   }
+// }
